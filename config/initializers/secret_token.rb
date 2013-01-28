a313b4df2fc10c3d164099ae7a3b82751ba2bd7d
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-01-29 00:40:29 +0530

    vanilla rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hummingbird::Application.config.secret_token = '5a1a381a19f810dabb98c79b9a56da5084745234e59c3c961579b329b8edd466a41fe1d367062b00356fcadf010fdd6f1fc324a617daba3016d708c8014d718f'
