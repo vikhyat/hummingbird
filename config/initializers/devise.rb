33f9e889bf9df56beeb55dff2f62e4f84c99bc96
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-07-14 08:07:16 +0530

    Add Facebook permissions.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -213,7 +213,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, '325314560922421', 'f686e6add09f2ac74140a287f40f6ff9'
+  config.omniauth :facebook, '325314560922421', 'f686e6add09f2ac74140a287f40f6ff9', scope: "publish_actions, email"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
