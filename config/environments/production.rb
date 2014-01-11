8f3d199ee2de586108f3e0880cbf09f88f123763
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-01-12 05:18:17 +0530

    Functionally equivalent prototype using React to render library entries.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Hummingbird::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.ember.variant = :production
+
+  config.react.variant = :production
 end
