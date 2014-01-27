3701c87deff17f94900b5226089d917eca86eaba
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-01-27 17:51:07 +0530

    Add secret key base.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Hummingbird::Application.config.secret_token = '5a1a381a19f810dabb98c79b9a56da5084745234e59c3c961579b329b8edd466a41fe1d367062b00356fcadf010fdd6f1fc324a617daba3016d708c8014d718f'
+Hummingbird::Application.config.secret_key_base = 'd78cd15a060c0ade966c2a2a90367a35f2b1446183fbfb58c574d5deed799dfb7796a905eabba652792110adc81051eb6cdf5afc55d196315ade84d489b985da'
